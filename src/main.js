b96e913a9493ea44c9d47f3da4d745d721fc79e4
wangchengfeng <wangchengfeng@xiaoyezi.com>
2022-06-16 14:24:41 +0800

    temp

--- src/main.js
+++ src/main.js
@@ -1,10 +1,47 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
-Vue.config.productionTip = false
+import vuex from 'vuex'
+Vue.use(vuex)
+const store = new vuex.Store({
+  modules: {
+    a: {
+      namespaced: true,
+      state: {
+        moduleACount: 10
+      },
+      mutations: {
+        changeModuleAState(state, count) {
+          state.moduleACount = count
+        }
+      }
+    }
+  },
+  state: {
+    count: 0,
+    inputValue: ''
+  },
+  mutations: {
+    increment(state, payload) {
+      state.count+=payload.count
+    },
+    updateInputValue(state, value) {
+      state.inputValue = value
+    }
+  },
+  actions: {
+    asyncIncrement(context, payload) {
+      setTimeout(() => {
+        context.commit('increment', {
+          count: payload.count
+        })
+      }, 1000);
+    }
+  }
+})
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>{{count}}</h1>
+    <h1 @click="$store.commit('a/changeModuleAState', 100)">{{$store.state.a}}</h1>
+    <input v-model="inputValue" type="text">
+    <h1>{{$store.state.inputValue}}</h1>
+    <!-- <button @click="$store.commit('increment')">add</button> -->
+    <button @click="asyncIncrement({count: 3})">add</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import {mapState, mapMutations, mapActions} from 'vuex'
 export default {
   name: 'Home',
+  computed: {
+    ...mapState(['count']),
+    inputValue: {
+      get() {
+        return this.$store.state.inputValue
+      },
+      set(newVal) {
+        this.$store.commit('updateInputValue', newVal)
+      }
+    }
+  },
   components: {
     HelloWorld
+  },
+  methods: {
+    ...mapMutations(['increment']),
+    ...mapActions(['asyncIncrement'])
+    // add() {
+    //   console.log(this.$store)
+    //   this.$store.commit('increment', {count: 2})
+    //   console.log(this.$store.state.count)
+    // }
   }
 }
 </script>
