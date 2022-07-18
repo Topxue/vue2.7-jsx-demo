c968fd3c062e9036309a30e0f81f63ab4d0e78a7
wangchengfeng <wangchengfeng@xiaoyezi.com>
2022-07-18 10:42:59 +0800

    refjsx get dom

--- src/components/refJsx.vue
+++ src/components/refJsx.vue
@@ -1,17 +1,28 @@
 <script>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 export default {
   setup() {
     const count = ref(0);
+    const refH1 = ref(null);
+    onMounted(() => {
+      console.log(refH1.value);
+      refH1.value.value = "hello world";
+    });
     setTimeout(() => {
       count.value = 12;
     }, 1000);
     return {
       count,
+      refH1,
     };
   },
   render(h) {
-    return <h1>{this.count ? <span>11</span> : <span>22</span>}</h1>;
+    return (
+      <div>
+        <h1>{this.count ? <span>11</span> : <span>22</span>}</h1>
+        <input ref="refH1" type="text"></input>
+      </div>
+    );
   },
 };
 </script>
